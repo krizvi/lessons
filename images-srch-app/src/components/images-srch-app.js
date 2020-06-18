52c3dc7fd57b4427cca01f9199b0079670e1c7c6
Khalid Rizvi <khalid.rizvi@icloud.com>
2020-06-18 07:43:51 -0400

    removed extra consoles

--- images-srch-app/src/components/images-srch-app.js
+++ images-srch-app/src/components/images-srch-app.js
@@ -7,7 +7,6 @@ const ImagesSrchApp = () => {
     const [responses, setResponses] = useState([]);
 
     const onSubmit = async (queryStr, imageSize) => {
-        console.log(queryStr, imageSize);
         const response = await photos.get('/search/photos', {
             params: {query: queryStr, size: imageSize},
         })
